c408a6fa2e5b3d12836f8d4d83550848277cb7cb
Elina <iskhakova.elina18@gmail.com>
2025-10-05 21:36:35 +0300

    Add import, fix flake8

--- bioinformatic_tool.py
+++ bioinformatic_tool.py
@@ -1,4 +1,10 @@
-def run_dna_rna_tools(*args: str):
+from modules.dna_rna_tools.py import (
+    is_nucleic_acid, reverse, transcribe, complement, reverse_complement
+    )
+from modules.fastq_tools.py import count_gc, count_quality
+
+
+def run_dna_rna_tools(*args: str) -> "bool | str":
     """
     Takes 1 or more sequences, checks if is it nucleic acid and
     does one of the tools: is_nucleic_acid, reverse, transcribe,
@@ -14,7 +20,7 @@ def run_dna_rna_tools(*args: str):
         Sequence is not an nucleic acid
         Tool is unknown
     """
-    # Проверяем, что аргументов минимум 2
+    # Checks if there are minimum 2 arguments
     if len(args) < 2:
         raise ValueError(
             "Введите хотя бы одну последовательность и одну процедуру")
@@ -22,7 +28,7 @@ def run_dna_rna_tools(*args: str):
     sequences = args[:-1]
     tool = args[-1]
 
-    # словарь тулов
+    # Tools dictionary
     tools = {
         "is_nucleic_acid": is_nucleic_acid,
         "transcribe": transcribe,
@@ -31,19 +37,71 @@ def run_dna_rna_tools(*args: str):
         "reverse_complement": reverse_complement,
     }
 
-    # Проверка, что процедура существует
+    # Checks if the tool exists
     if tool not in tools:
         raise ValueError("Неизвестная процедура")
 
-    # Проверка, что последовательность - нуклеиновая кислота
+    #  Checks if the sequence is nucleic acid
     for seq in sequences:
         if tool != "is_nucleic_acid" and not is_nucleic_acid(seq):
             raise ValueError("Введена некорректная последовательность")
 
-    # Выполнение функции
+    #  Resuls of the fuction
     results = [tools[tool](seq) for seq in sequences]
-
-    # Условие, чтобы при подаче 1 последовательности возвращалась строка
     if len(results) == 1:
         return results[0]
     return results
+
+
+def filter_fastq(
+    seqs: dict,
+    gc_bounds: "tuple | int | float" = (0, 100),
+    length_bounds: "tuple | int | float" = (0, 2**32),
+    quality_threshold: float = 0
+) -> dict:
+    """
+    Filters reads in fastq file, according to a number of
+    guanine-cytosine bounds, length of a read and mean quality
+    of a read.
+
+    Arguments:
+    seqs: dict[str, tuple[str, str]]
+    gc_bounds: tuple/int/float (default (0, 100))
+    length_bounds: tuple/int/float (default (0, 2**32))
+    quality_threshold: int/float (default 0)
+
+    Returns dict[str, tuple[str, str]]
+    """
+
+# Checks the gc_bounds type and defines the range of GC-bounds
+    if isinstance(gc_bounds, (int, float)):
+        min_gc = 0
+        max_gc = gc_bounds
+    elif isinstance(gc_bounds, tuple):
+        min_gc = gc_bounds[0]
+        max_gc = gc_bounds[1]
+    else:
+        raise ValueError("Incorrect interval for GC-bounds")
+
+# Checks the length_bounds type and defines the range of sequence's length
+    if isinstance(length_bounds, (int, float)):
+        min_length = 0
+        max_length = length_bounds
+    elif isinstance(length_bounds, tuple):
+        min_length = length_bounds[0]
+        max_length = length_bounds[1]
+    else:
+        raise ValueError("Incorrect interval for length")
+
+# Filters the reads by input parametrs
+    filtered_fastq = {}
+    for key, value in seqs.items():
+        seq, qual = value
+        if (
+            (min_gc <= count_gc(seq) <= max_gc)
+            and (min_length <= len(seq) <= max_length)
+            and count_quality(qual) > quality_threshold
+        ):
+            filtered_fastq[key] = (seq, qual)
+
+    return filtered_fastq
