fd7a3b605506b73c9e87d2615dd22a677d12a2db
Elina <iskhakova.elina18@gmail.com>
2025-10-12 15:24:34 +0300

    Add read_fastq and write fastq in modules

--- modules/fastq_tools.py
+++ modules/fastq_tools.py
@@ -1,3 +1,5 @@
+import os
+
 def count_gc(seq: str) -> float:
     """
     Counts number of guanine-cytosine bounds in sequence.
@@ -26,3 +28,51 @@ def count_quality(qual: str) -> float:
     for char in qual:
         char_quality += ord(char) - 33
     return round(char_quality/len(qual), 2)
+
+
+def read_fastq(input_fastq: str) -> dict:
+    """
+    Reads the input fastq file and turns it into the dictionary:
+    key: ID of sequence; value: (sequence, quality).
+
+    Arguments:
+    input_fastq: str
+
+    Returns dict[str, tuple[str, str]]
+    """
+    seqs = {}
+    with open(input_fastq) as fastq_file:
+        while True:
+            id = fastq_file.readline()
+            if not id:
+                break
+            seq = fastq_file.readline()
+            plus = fastq_file.readline()
+            qual = fastq_file.readline()
+            seqs[id.strip()] = (seq.strip(), qual.strip())
+    return seqs
+
+
+def write_fastq(filtered_fastq: dict, output_fastq: str) -> str:
+    """
+    Writes the filtered dictionary of sequences into the fastq file.
+    Makes 'filtered' directory, if there are no such directory.
+    Does not rewrite output_fastq file, if it already exists.
+
+    Arguments:
+    filtered_fastq: dict
+    output_fastq: str
+
+    Returns str
+    Raises exception if output_fastq file already exists.
+    """
+    if not os.path.exists("filtered"):
+        os.mkdir("filtered")
+    output_path = os.path.join("filtered", output_fastq)
+    if os.path.exists(output_path):
+        print("File output_fastq already exists")
+        return
+
+    with open(output_path, "w") as fastq_file:
+        for id, (seq, qual) in filtered_fastq.items():
+            fastq_file.write(f"{id}\n{seq}\n + \n{qual}\n")
